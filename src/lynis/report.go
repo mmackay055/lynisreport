e1f8681a42d7e31d380938e902ec9eeaf3ca5c0f
mmackay055 <mmackay055@gmail.com>
2022-04-01 16:02:51 -0600

    added better error handling when setting fields of TestElement

--- src/lynis/report.go
+++ src/lynis/report.go
@@ -3,10 +3,11 @@ package lynis
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"io"
 	"regexp"
+	"strconv"
 	"strings"
-        "fmt"
 )
 
 const (
@@ -14,13 +15,19 @@ const (
 )
 
 const (
+	KEY_LYNISVER              string = `lynis_version`
 	KEY_WARNING               string = `warning[]`
 	KEY_SUGGESTION            string = `suggestion[]`
 	KEY_REPORT_DATETIME_START string = `report_datetime_start`
 	KEY_REPORT_DATETIME_END   string = `report_datetime_end`
 )
 
+const (
+        VER string = "3.0.7"
+)
+
 type Report struct {
+	LynisVersion  string           `json:"lynisVersion"`
 	DateTimeStart string           `json:"DateTimeStart"`
 	DateTimeEnd   string           `json:"DateTimeEnd"`
 	Tests         map[string]*Test `json:"tests"`
@@ -44,6 +51,34 @@ func CreateReport(input io.Reader) (*Report, error) {
 	return report, nil
 }
 
+func CheckVersion(ver string) error {
+        vCheck := strings.Split(ver, ".")
+        vValid := strings.Split(VER, ".")
+        if len(vCheck) != 3 {
+                return errors.New("Lynis version string invalid format")
+        }
+
+        // check all version numbers
+        for i, v := range vCheck {
+                check,err := strconv.Atoi(v)
+                if err != nil {
+                        return errors.New("Lynis version string contains non number")
+                }
+                valid,err := strconv.Atoi(vValid[i])
+                if err != nil {
+                        panic(err)
+                }
+                if check < valid {
+                        return errors.New("Lynis version is not compatable")
+                } else if check > valid {
+                        break // version is greater than valid version
+                }
+                // continue checking if version number is equal
+        }
+        
+       return nil 
+}
+
 func (r *Report) ProcessLine(line string) error {
 	// skip lines commented with '#' or empty
 	if r.nonline.Match([]byte(line)) {
@@ -55,15 +90,20 @@ func (r *Report) ProcessLine(line string) error {
 		return nil
 	}
 
-        if err := r.Add(key, value); err != nil {
-                return err
-        }
+	if err := r.Add(key, value); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (r *Report) Add(key, value string) error {
-        var err error
+	var err error
 	switch key {
+	case KEY_LYNISVER:
+                if err := CheckVersion(value); err != nil {
+                        return err
+                }
+                r.LynisVersion = value
 	case KEY_WARNING:
 		_, err = r.parseTestValues(value, &Warning{}, AddWarning)
 	case KEY_SUGGESTION:
@@ -90,15 +130,15 @@ func (r *Report) AddTest(name string) *Test {
 }
 
 func (r *Report) parseTestValues(value string, te TestElement, add func(*Test, TestElement)) (*Test, error) {
-        values := strings.Split(value, "|")
-        values = values[:len(values) - 1] // remove last element which is blank
+	values := strings.Split(value, "|")
+	values = values[:len(values)-1] // remove last element which is blank
 
-        if len(values) < 2 {
+	if len(values) < 2 {
 		return nil, errors.New("malformed line no test name or test is missing info")
 	}
 
 	// set the fields of the TestElement
-	if err := te.setFields(values[1:]); err != nil {
+	if err := SetFields(values[1:], te); err != nil {
 		return nil, err
 	}
 
@@ -129,14 +169,14 @@ func (r *Report) Process(input io.Reader) error {
 	bufinput := bufio.NewReader(input)
 
 	// process all lines in file
-        for i := 1;;i++ {
+	for i := 1; ; i++ {
 		line, readerr := bufinput.ReadString('\n')
 		if readerr != nil && readerr != io.EOF {
 			return readerr
 		}
 
 		if err := r.ProcessLine(line); err != nil {
-                        return errors.New(fmt.Sprintf("line %d: %s", i, err))
+			return errors.New(fmt.Sprintf("line %d: %s", i, err))
 		}
 
 		if readerr == io.EOF {
--- src/lynis/testelement.go
+++ src/lynis/testelement.go
@@ -2,45 +2,54 @@ package lynis
 
 import (
 	"errors"
+	"strings"
+        "reflect"
 )
+
+//TODO add error type for setFields function to only issue as a warning
 
 type TestElement interface {
 	setFields(values []string) error
 }
 
+func SetFields(values []string, te TestElement) error {
+        length := len(values)
+	if length != 3 && length >= 1 {
+                // just set first field joined with other fields
+                reflect.ValueOf(te).Elem().Field(0).SetString(strings.Join(values, "|"))
+
+                //TODO send warning message as specific error
+		return nil
+	} else if length != 3 {
+                return errors.New("element does not have correct amount of fields")
+        }
+
+        // set TestElement fields
+        return te.setFields(values) 
+}
 type Warning struct {
 	Message string `json:"message"`
 	Object  string `json:"object"`
 	Misc    string `json:"misc"`
 }
 
+
 func (w *Warning) setFields(values []string) error {
-	if len(values) != 3 {
-		return errors.New("warning is missing fields")
-	}
-        
-	w.Message = values[0]
-	w.Object  = values[1]
-	w.Misc    = values[2]
-        
+        w.Message = values[0]
+        w.Object = values[1]
+        w.Misc = values[2]
 	return nil
 }
 
 type Suggestion struct {
-	Message string `json:"message"`
-	Misc1   string `json:"misc1"`
-	Misc2   string `json:"misc2"`
+	Message  string `json:"message"`
+	Details  string `json:"details"`
+	Solution string `json:"solution"`
 }
 
 func (s *Suggestion) setFields(values []string) error {
-	if len(values) != 3 {
-		return errors.New("suggestion is missing fields")
-	}
-
-	s.Message = values[0]
-	s.Misc1   = values[1]
-	s.Misc2   = values[2]
-
+        s.Message = values[0]
+        s.Details = values[1]
+        s.Solution = values[2]
 	return nil
-
 }
