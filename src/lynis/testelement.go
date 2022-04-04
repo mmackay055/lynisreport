a99405954dce88ff1323e7f83fd16b677efb4a30
mmackay055 <mmackay055@gmail.com>
2022-04-04 10:29:54 -0600

    change json names

--- src/lynis/testelement.go
+++ src/lynis/testelement.go
@@ -7,22 +7,22 @@ import (
 
 //TODO add error type for setFields function to only issue as a warning
 type TestElement struct {
-        Message string `json:"message"`
-	Object  string `json:"object"`
-	Misc    string `json:"misc"`
+	Message  string `json:"message"`
+	Details  string `json:"details"`
+	Solution string `json:"solution"`
 }
 
 func NewTestElement(values []string) (*TestElement, error) {
-        length := len(values)
+	length := len(values)
 	if length != 3 && length >= 1 {
-                // just set message field joined with other fields
-                //TODO send warning message as specific error
-                return &TestElement{
-                        Message: strings.Join(values, "|"),
-                }, nil
+		// just set message field joined with other fields
+		//TODO send warning message as specific error
+		return &TestElement{
+			Message: strings.Join(values, "|"),
+		}, nil
 	} else if length != 3 {
-                return nil,errors.New("element does not have correct amount of fields")
-        }
+		return nil, errors.New("element does not have correct amount of fields")
+	}
 
-        return &TestElement {values[0], values[1], values[2]}, nil
+	return &TestElement{values[0], values[1], values[2]}, nil
 }
